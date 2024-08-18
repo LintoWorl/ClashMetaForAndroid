5ff9859cb713014fa67c1be3648a83cc499e707b
Link2World <plat2foreign98@proton.me>
2024-08-18 15:22:04 +0800

    fix a potential NPE

--- design/src/main/java/com/github/kr328/clash/design/view/CenteredCheckBox.kt
+++ design/src/main/java/com/github/kr328/clash/design/view/CenteredCheckBox.kt
@@ -10,20 +10,21 @@ import androidx.appcompat.widget.AppCompatCheckBox
 class CenteredCheckBox : AppCompatCheckBox {
     constructor(context: Context) : super(context)
     constructor(context: Context, attrS: AttributeSet) : super(context, attrS)
-
     constructor(context: Context, attrS: AttributeSet, defStyAttr: Int)
             : super(context, attrS, defStyAttr)
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         val drawables = getCompoundDrawables()
-        val drawable = drawables[0]
+        val drawable = drawables[0] ?: drawables[2]
         val gravity = gravity
         var left = 0
-        if (gravity == Gravity.CENTER) {
-            left =
-                (width - drawable.intrinsicWidth - paint.measureText(getText().toString())).toInt() / 2
+        drawable?.apply {
+            if (gravity == Gravity.CENTER) {
+                left =
+                    (width - intrinsicWidth - paint.measureText(getText().toString())).toInt() / 2
+            }
+            setBounds(left, 0, left + intrinsicWidth, intrinsicHeight)
         }
-        drawable.setBounds(left, 0, left + drawable.intrinsicWidth, drawable.intrinsicHeight)
     }
 }
