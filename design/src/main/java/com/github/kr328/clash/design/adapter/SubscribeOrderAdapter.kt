b73161500810cadf1a82a994a50fc6a8583d6e3e
Link2World <plat2foreign98@proton.me>
2024-09-09 16:48:12 +0800

    modify a data's display bug

--- design/src/main/java/com/github/kr328/clash/design/adapter/SubscribeOrderAdapter.kt
+++ design/src/main/java/com/github/kr328/clash/design/adapter/SubscribeOrderAdapter.kt
@@ -6,11 +6,12 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import app.hw.network.model.OrderBean
 import app.hw.network.model.OrderStatus
+import com.github.kr328.clash.common.log.Logger
 import com.github.kr328.clash.common.util.TimeFormat
 import com.github.kr328.clash.design.databinding.AdapterSubscribeOrderBinding
+import com.github.kr328.clash.design.util.formatPrice
 import com.github.kr328.clash.design.util.layoutInflater
 import com.github.kr328.clash.design.util.onClickNew
-import java.text.DecimalFormat
 
 /**
  * 我的订单管理界面的列表
@@ -33,11 +34,30 @@ class SubscribeOrderAdapter(val context: Context, val clickedItem: (OrderBean) -
         val orderBean = orderBeans[position]
         holder.binding.tvSubsOrderTitle.text =
             orderBean.plan?.name?.takeIf { it.isNotEmpty() } ?: "默认套餐"
-        val df = DecimalFormat("#.00")
-        val priceVal = orderBean.total_amount / 100f
-        holder.binding.tvSubsOrderPrice.text =
-            "¥ " + if (priceVal < 1.0f) "0${df.format(priceVal)}" else df.format(priceVal)
-        //holder.binding.tvSubsOrderPrice.text = "¥ " + df.format(orderBean.total_amount / 100f)
+        val payCycle = orderBean.period
+        Logger.d("the period of subscribe is:$payCycle")
+        orderBean.plan?.apply {
+            when (payCycle) {
+                "month_price" -> holder.binding.tvSubsOrderPrice.text =
+                    month_price?.let { (it / 100f).formatPrice() }
+
+                "quarter_price" -> holder.binding.tvSubsOrderPrice.text =
+                    quarter_price?.let { (it / 100f).formatPrice() }
+
+                "half_year_price" -> holder.binding.tvSubsOrderPrice.text =
+                    half_year_price?.let { (it / 100f).formatPrice() }
+
+                "year_price" -> holder.binding.tvSubsOrderPrice.text =
+                    year_price?.let { (it / 100f).formatPrice() }
+
+                "two_year_price" -> holder.binding.tvSubsOrderPrice.text =
+                    two_year_price?.let { (it / 100f).formatPrice() }
+
+                "three_year_price" -> holder.binding.tvSubsOrderPrice.text =
+                    three_year_price?.let { (it / 100f).formatPrice() }
+            }
+        }
+
         holder.binding.tvSubsOrderTime.text = TimeFormat.millis2String(orderBean.created_at * 1000L)
         holder.binding.tvSubsOrderState.text = OrderStatus().getStatusDesc(orderBean.status)
         holder.binding.root.onClickNew {
