c65e727a2aad9409b41d4bc5cb47a95850bbf393
Link2World <plat2foreign98@proton.me>
2025-11-15 11:30:31 +0800

    fix dialog content's display format

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,8 @@ subprojects {
             targetSdk = 34
 
             versionName =
-                "v2.10.0_b" + SimpleDateFormat("yyMMdd").format(Date())
-            versionCode = 2021000
+                "v2.10.1_b" + SimpleDateFormat("yyMMdd").format(Date())
+            versionCode = 2021001
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
