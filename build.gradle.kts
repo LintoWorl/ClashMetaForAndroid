b0b51dd1a0c2f8552cc837e1b49818f4896dc939
Link2World <plat2foreign98@proton.me>
2025-12-27 19:14:15 +0800

    update app's version

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,8 @@ subprojects {
             targetSdk = 34
 
             versionName =
-                "v1.3.2_b" + SimpleDateFormat("yyMMdd").format(Date())
-            versionCode = 1010302
+                "v1.3.3_b" + SimpleDateFormat("yyMMdd").format(Date())
+            versionCode = 1010303
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
